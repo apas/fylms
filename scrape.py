276f1cb1e3d513563c8413b23a716d2b31aba9e6
apas <apostolos.me@gmail.com>
2016-11-20 13:40:50 +0100

    reaches alpha version, rearranges some code

--- scrape.py
+++ scrape.py
@@ -46,10 +46,6 @@ def create_dict(films):
 
   return d
 
-artis_dict = create_dict(artis_films)
-apollo_dict = create_dict(apollo_films)
-village_dict = create_dict(village_films)
-
 def get_ids(dicts):
   for k, v in dicts.iteritems():
     try:
@@ -58,13 +54,62 @@ def get_ids(dicts):
     except ValueError:
       pass
 
+# provided with an imdb id letterboxd redirects to film page
+def get_rating(input):
+  movie_url = "http://letterboxd.com/imdb/" + input
+  movie = requests.get(movie_url)
+  tree_movie = html.fromstring(movie.content.encode('utf-16'))
+  xpath_str2 = "//*[@id=\"film-page-wrapper\"]/div[2]/aside/section[2]\
+  /span/span/text()"
+  movie_rating = str(tree_movie.xpath(xpath_str2)[0])
+  return movie_rating
+
+def append_ratings(dicts):
+  for k, v in dicts.iteritems():
+    for val in v:
+      if val.startswith("tt"):
+        dicts[k].append(get_rating(val))
+
+# print "\n##########################################\n"
+# print json.dumps(artis_dict, ensure_ascii=False, indent=2)
+# print "\n##########################################\n"
+# print json.dumps(apollo_dict, ensure_ascii=False, indent=2)
+# print "\n##########################################\n"
+# print json.dumps(village_dict, ensure_ascii=False, indent=2)
+
+def print_data(dicts):
+  for k, v in dicts.iteritems():
+    rating = 0.0
+    times = ""
+    for val in v:
+      if not isTime(val) and not val.startswith("tt"):
+        if float(val) >= 3.2:
+          rating = float(val)
+      if isTime(val):
+        times = times + " " + val + " "
+
+    if rating >= 3.2:
+      print "\t" + k
+      print "\t" + times[1:]
+      print "\t" + "Rating: " + str(rating)
+      print "\n"
+
+artis_dict = create_dict(artis_films)
+apollo_dict = create_dict(apollo_films)
+village_dict = create_dict(village_films)
+
 get_ids(artis_dict)
 get_ids(apollo_dict)
 get_ids(village_dict)
 
-# print "\n##########################################\n"
-print json.dumps(artis_dict, ensure_ascii=False, indent=2)
-print "\n##########################################\n"
-print json.dumps(apollo_dict, ensure_ascii=False, indent=2)
-print "\n##########################################\n"
-print json.dumps(village_dict, ensure_ascii=False, indent=2)
+append_ratings(artis_dict)
+append_ratings(apollo_dict)
+append_ratings(village_dict)
+
+print "Today's movie recommendations:\n"
+print "@ Artis International Cinema"
+print_data(artis_dict)
+print "@ Apollo Cinema"
+print_data(apollo_dict)
+print "@ Village Wien Mitte"
+print_data(village_dict)
