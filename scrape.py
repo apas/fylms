1cf834e8c06d2d65ba7755e74cd81307f33ed495
apas <apostolos.me@gmail.com>
2016-11-23 18:47:51 +0100

    scrapes Haydn cinema & supports logging

--- scrape.py
+++ scrape.py
@@ -1,12 +1,18 @@
 import requests
-import sys, time, json, datetime, locale, itertools
+import sys, time, json, datetime, locale, itertools, logging
 from lxml import html, etree
 from collections import defaultdict
 import terminalmdb as imdb
 import config
 
 reload(sys) # helpers for utf-8
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding("utf-8")
+logger = logging.getLogger("scrape")
+hdlr = logging.FileHandler("scrape.log")
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.WARNING)
 
 # return True if string is time HH:MM
 def isTime(input):
@@ -23,16 +29,19 @@ apollo_url = "http://www.cineplexx.at/service/program.\
 php?type=program&centerId=8&originalVersionTypeFilter=OV"
 village_url = "http://www.cineplexx.at/service/program.\
 php?type=program&centerId=115&originalVersionTypeFilter=OV"
+haydn_url = "http://www.haydnkino.at/Cinema/OverviewList?tabSelected=0"
 
 # load the url
 artis = requests.get(artis_url)
 apollo = requests.get(apollo_url)
 village = requests.get(village_url)
+haydn = requests.get(haydn_url)
 
 # construct tree out of html
 tree_artis = html.fromstring(artis.content.encode('utf-16'))
 tree_apollo = html.fromstring(apollo.content.encode('utf-16'))
 tree_village = html.fromstring(village.content.encode('utf-16'))
+tree_haydn = html.fromstring(haydn.content.encode('utf-16'))
 
 # access the elements we want with `or` constructor
 # h2/a/ = film name
@@ -42,6 +51,14 @@ xpath_str = "//div[@class='overview-element separator']//h2//a/text() \
 artis_films = tree_artis.xpath(xpath_str)
 apollo_films = tree_apollo.xpath(xpath_str)
 village_films = tree_village.xpath(xpath_str)
+
+xpath_str_haydn = "//div[@id='tabsAll']//table[@class='movie']//span[\
+@class='title teen_bold']/text() | //div[@id='tabsAll']//div[@class=\
+'events']//a/text()"
+haydn_films = tree_haydn.xpath(xpath_str_haydn)
+
+for film in haydn_films:
+  film = film.replace("\n", "")
 
 # return a <film, [showtimes]> dictionary out of the accessed films
 def create_dict(films):
@@ -73,8 +90,12 @@ def get_rating(input):
   tree_movie = html.fromstring(movie.content.encode('utf-16'))
   xpath_str2 = "//*[@id=\"film-page-wrapper\"]/div[2]/aside/section[2]\
   /span/span/text()"
-  movie_rating = str(tree_movie.xpath(xpath_str2)[0])
-  return movie_rating
+  try:
+    movie_rating = str(tree_movie.xpath(xpath_str2)[0])
+    return movie_rating
+  except Exception, e:
+    logger.error(e)
+    return "0"
 
 # append letterboxd rating in dictionary as extra value
 def append_ratings(dicts):
@@ -124,18 +145,22 @@ def prepare_data():
   global artis_dict # ugly globals but print_movies() needs access
   global apollo_dict
   global village_dict
+  global haydn_dict
 
   artis_dict = create_dict(artis_films)
   apollo_dict = create_dict(apollo_films)
   village_dict = create_dict(village_films)
+  haydn_dict = create_dict(haydn_films)
 
   get_ids(artis_dict)
   get_ids(apollo_dict)
   get_ids(village_dict)
+  get_ids(haydn_dict)
 
   append_ratings(artis_dict)
   append_ratings(apollo_dict)
   append_ratings(village_dict)
+  append_ratings(haydn_dict)
 
 # return all movie recommendations for all theaters in a string
 def print_movies():
@@ -147,6 +172,8 @@ def print_movies():
   res = res + print_data(apollo_dict)
   res = res + "@ Village Wien Mitte" + "\n"
   res = res + print_data(village_dict)
+  res = res + "@ Haydn Kino" + "\n"
+  res = res + print_data(haydn_dict)
 
   return res 
 
